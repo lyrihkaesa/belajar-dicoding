ff435ae2526c1dc86615a5d0812eaf82f7c8f22e
lyrihkaesa <kaesalyrih@gmail.com>
2023-03-19 18:36:51 +0700

    add: latihan belajar android pemula

---
+++ android-learning-path/belajar-membuat-aplikasi-android-untuk-pemula/MyIntentApp/app/src/main/java/com/dicoding/myintentapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.dicoding.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMoveActivity = findViewById(R.id.btn_move_activity);
+
+        btnMoveActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_move_activity) {
+            Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+            startActivity(moveIntent);
+        }
+    }
+}
